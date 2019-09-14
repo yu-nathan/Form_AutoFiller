41fb72af30a9ca613d88d5c0daf838b1d5e8178c
Nathan Yu <nyu1997@gmail.com>
2019-09-14 03:17:03 -0700

    Quick bug fix and added pattern for lever.co

--- autoFormFiller.py
+++ autoFormFiller.py
@@ -5,23 +5,29 @@ import re
 
 def main():
     # setting up command line argument parser
-    parser = argparse.ArgumentParser(description="Autofill some forms.")
-    parser.add_argument("url", type=str, nargs=1,
-                        help="the website to autofill forms for")
+    parser = argparse.ArgumentParser(description='Autofill some forms.')
+    parser.add_argument('url', type=str, nargs=1,
+                        help='the website to autofill forms for')
     #parser.add_argument()
     args = parser.parse_args()
 
     # sets url to the one provided in the command line
     url = args.url[0]
+
+    # checks url to see which application website we are using
+    lvrPttrn = r'https://jobs.lever.co/.+'
+    lvrMatch = re.search(lvrPttrn, url)
+
+    # initialize browser object
     br = mechanize.Browser()
-    header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
-    br.set_header("User-agent", header)
+    header = Mozilla/5.0 ('Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
+    br.set_header('User-agent', header)
     response = br.open(url)
     br.form = br.forms()[0]
     #print(br.form.controls[1].__dict__)
     #br.submit()
     for control in br.form.controls:
-       print("name: %s, type: %s" % (control.name, control.type))
+       print('name: %s, type: %s' % (control.name, control.type))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
